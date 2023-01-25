beca7e6ab6ea3f718d248d20d11534679d9dc4a4
Bástyai Botond <64700017+botator@users.noreply.github.com>
2023-01-25 10:04:13 +0100

    Add files via upload

---
+++ elso.js
@@ -0,0 +1,16 @@
+
+/*function osszeadas()
+{
+for(let i =0;i<10;i++)
+{
+   
+let a = 0;
+let b = 0;
+let answer =0;
+a = Math.floor(Math.random() * (1000-10)) + 10;
+b = Math.floor(Math.random() * (1000-10)) + 10;
+document.write(a+" + "+b+" = "+a+b)
+
+}
+}
+osszeadas()*/
